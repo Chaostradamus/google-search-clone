bde1739dc6ebb3178b42fa5b4f45cddbc20affb2
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-09-27 02:22:02 -0400

    post results

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("Street Fighter");
 
   //   /videos, /search, /images
   const getResults = async (type) => {
@@ -21,16 +21,25 @@ export const ResultContextProvider = ({ children }) => {
         "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
       },
     });
-    
+
     const data = await response.json();
-    setResults(data);
+
+    if (type.includes("/news")) {
+      setResults(data.entries)
+    } else if (type.includes("/images")) {
+      setResults(data.image_results);
+    } else {
+     setResults(data.results);
+    }
     setIsLoading(false);
   };
   return (
     <ResultContext.Provider
       value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
-    >{children}</ResultContext.Provider>
+    >
+      {children}
+    </ResultContext.Provider>
   );
 };
 
-export const useResultContext = () => useContext(ResultContext)
+export const useResultContext = () => useContext(ResultContext);
